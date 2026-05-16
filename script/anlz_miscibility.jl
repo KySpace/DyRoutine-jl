3a64d028699cdc56b620539a3526595c4c143b42
ky@PC25 <wangky1024@outlook.com>
2026-05-16 09:04:14 +0800

    miscibility display progress

---
+++ script/anlz_miscibility.jl
@@ -0,0 +1,20 @@
+using HDF5
+using CairoMakie: Figure, Axis, Colorbar, DataAspect, heatmap!, lines!, scatter!, save, text!, rowgap!, colgap!
+using GLMakie
+using JLD2
+using Printf
+GLMakie.activate!()
+include(joinpath(@__DIR__, "..", "src", "helper.jl"))
+include(joinpath(@__DIR__, "..", "src", "persolo.jl"))
+include(joinpath(@__DIR__, "..", "src", "percond.jl"))
+include(joinpath(@__DIR__, "..", "src", "graphics.jl"))
+include(joinpath(@__DIR__, "..", "src", "corr.jl"))
+include(joinpath(@__DIR__, "..", "src", "vissolo.jl"))
+include(joinpath(@__DIR__, "..", "src", "viscorr.jl"))
+include(joinpath(@__DIR__, "..", "src", "vispca.jl"))
+
+year_test = 2026
+path_root = raw"C:\Users\ky\OneDrive\Source Shared\DyGist\Data\Excitations"
+runinfos = [
+    (date="0513", runids=71, IB=5.311, tag_head="Evol"),
+]
--- src/visduet.jl
+++ src/visduet.jl
@@ -6,6 +6,26 @@ using CairoMakie, GLMakie
 using Colors: Oklch
 using LaTeXStrings
 
+# intend to layout a grid of axes with 2 main variables
+# time varies in the horizontal direction, another variable vert varies in the vertical direction
+# possible repetitions are all grouped together, horizontally
+# dimensions have to be specified as (n_reps, n_vert, n_time, n_istp)
+function set_axes_v_t_rep!(n_dim_vars::Tuple{<:Integer,<:Integer,<:Integer,<:Integer}, panel_setter::Function, runinfo, info_fmt)
+    fig = Figure()
+    fig[0, 1] = Label(fig, text="$(runinfo.date) run$(runinfo.runids) IB=$(@sprintf("%.3f", runinfo.IB))A $(runinfo.tag_head)"; tellwidth=false, tellheight=true, halign=:left, valign=:top)
+    axs = Array{Dict}(undef, n_dim_vars[1:3]...)
+    for v in 1:n_dim_vars[2], t in 1:n_dim_vars[3]
+        fig[v, t][0, 1:n_dim_vars[1]] = Label(fig, text="bias=$(info_fmt[1,v,t,1].bias), t hold=$(info_fmt[1,v,t,1].t_hold) ms)"; tellwidth=true, tellheight=true, halign=:center, valign=:bottom)
+        for r in 1:n_dim_vars[1]
+            gl = GridLayout()
+            fig[v, t][1, r] = gl
+            axs[r, v, t] = panel_setter(gl)
+        end
+        fig[v, t].layout |> l -> colgap!(l, 0)
+    end
+    return fig, axs
+end
+
 function set_panel_misc_duet_2d!(gl::GridLayout)
     gl |> clean_gridlayout!
     ax_dens_1 = Axis(gl[1, 1])
@@ -20,7 +40,7 @@ end
 function to_miscibility_clr(dens1, dens2, hue1, hue2; max=16, to_norm_each=false)
     size(dens1) == size(dens2) || throw(ArgumentError("dens1 and dens2 must have the same size"))
     dens_norm_1, dens_norm_2 = (dens1, dens2) |> d -> clamp.(d, 0, max)
-    shader = (a, b)Oklch(1 - (a + b) / 2, abs(a - b) * 0.24, a > b ? hue1 : hue2) |> RGBf
+    shader = (a, b) -> Oklch(1 - (a + b) / 2, abs(a - b) * 0.24, a > b ? hue1 : hue2) |> RGBf
     return [shader(dens_norm_1[x, y], dens_norm_2[x, y]) for x in 1:size(dens1, 1), y in 1:size(dens1, 2)]
 end
 
