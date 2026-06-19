71ac50dfbf5280447cc0d5f37785bdca91e6d34f
ky@PC25 <wangky1024@outlook.com>
2026-06-20 01:45:24 +0800

    Some basics tests for masked prfl

--- script/test_fringe_sidepeak.jl
+++ script/test_fringe_sidepeak.jl
@@ -4,16 +4,24 @@ include(joinpath(@__DIR__, "..", "src", "vissolo.jl"))
 ## function definitions
 function set_panel_solo_modl_masks!(gl::GridLayout)
     gl |> clean_gridlayout!
+    kwargs_label = (;tellwidth=false, tellheight=false, halign=:left, valign=:bottom)
     label = Label(gl[0, 1:5]; tellwidth=false, tellheight=false, halign=:left, valign=:bottom)
-    ax_dens_core = Axis(gl[1, 1], width=120, height=240)
-    ax_dens_core_masked_1 = Axis(gl[1, 2], width=120, height=240)
-    ax_dens_core_masked_2 = Axis(gl[1, 3], width=120, height=240)
-    ax_dens_core_masked_in = Axis(gl[1, 4], width=120, height=240)
-    ax_modl = Axis(gl[1, 5], width=120, height=240)
-    ax_prfl_ft_upright = Axis(gl[1, 6], width=240, height=240)
+    Label(gl[1, 1]; text="density", kwargs_label...)
+    Label(gl[1, 2]; text="fringe", kwargs_label...)
+    Label(gl[1, 3]; text="envelop", kwargs_label...)
+    Label(gl[1, 4]; text="masked modulation", kwargs_label...)
+    Label(gl[1, 5]; text="FT 2D", kwargs_label...)
+    Label(gl[1, 6]; text="FT profile", kwargs_label...)
+    ax_dens_core = Axis(gl[2, 1], width=120, height=240)
+    ax_dens_core_masked_1 = Axis(gl[2, 2], width=120, height=240)
+    ax_dens_core_masked_2 = Axis(gl[2, 3], width=120, height=240)
+    ax_dens_core_masked_in = Axis(gl[2, 4], width=120, height=240)
+    ax_modl = Axis(gl[2, 5], width=120, height=240)
+    ax_prfl_ft_upright = Axis(gl[2, 6], width=240, height=240)
     colgap!(gl, 5)
     rowgap!(gl, 2)
-    rowsize!(gl, 0, 4)
+    rowsize!(gl, 0, 16)
+    rowsize!(gl, 1, 16)
     return Dict(
         "dens_core" => ax_dens_core,
         "dens_masked_1" => ax_dens_core_masked_1,
@@ -24,7 +32,7 @@ function set_panel_solo_modl_masks!(gl::GridLayout)
         "label" => label)
 end
 
-function to_masked_clr(dens, mask, hue; sat_max=0.24, max=16, thres_alpha=0.1, l_max=0.8, l_min=0.0, alpha_base=0.1)
+function to_masked_clr(dens, mask, hue; sat_max=0.24, max=16, thres_alpha=0.1, l_max=1.0, l_min=0.0, alpha_base=0.1)
     size(dens) == size(mask) || throw(ArgumentError("dens and mask must have the same size"))
     norm = d -> clamp.(d, 0, max) / max
     dens_norm = dens |> norm
@@ -34,7 +42,7 @@ function to_masked_clr(dens, mask, hue; sat_max=0.24, max=16, thres_alpha=0.1, l
     return [shader(dens_norm[x, y], mask[x, y]) for x in 1:size(dens, 1), y in 1:size(dens, 2)]
 end
 
-function draw_solo_modl_mask!(axs::Dict{String}, extr::SoloExtract, info_solo, mask1, mask2; dens_max=16.0, peak_height_max=2)
+function draw_solo_modl_mask!(axs::Dict{String}, extr::SoloExtract, info_solo, mask1, mask2, mask_prfl, prfls_masked, fit_tail; dens_max=16.0, peak_height_max=2)
 
     isnothing(extr.envelope) && return
     isnothing(extr.sidepeak) && return
@@ -43,21 +51,24 @@ function draw_solo_modl_mask!(axs::Dict{String}, extr::SoloExtract, info_solo, m
     foreach(a -> a isa Axis && empty!(a), values(axs))
     essn = extr.essentials
     mask_hann = gen_win_hann_2d(essn.smwh_core)
-    norm_modl = m -> map(p -> clamp(p, 0, Inf), m) |> m -> m ./ (sum(m) * (essn.step_modl[2] / 2)^2)
+    norm_modl = m -> m ./ (sum(essn.modl2d) * (essn.step_modl[2] / 2)^2)
+    # norm_modl_offset = m -> clamp(m, 0, Inf) |> m -> m ./ (sum(m) * (essn.step_modl[2] / 2)^2)
     modl2d_norm = essn.modl2d |> norm_modl
-    calc_dens_mask = mask -> (essn.dens2d_core .* mask_hann |> fft |> fftshift |> m -> m .* mask |> ifftshift |> ifft |> d -> real.(d) |> norm_modl)
-    dens_masked_1 = calc_dens_mask(mask1)
-    dens_masked_2 = calc_dens_mask(mask2)
+    calc_dens_mask = mask -> (essn.dens2d_core .* mask_hann |> fft |> fftshift |> m -> m .* mask |> ifftshift |> ifft |> d -> real.(d))
+    dens_masked_1 = mask1 |> calc_dens_mask |> m -> clamp.(m, 0, Inf)
+    dens_masked_2 = mask2 |> calc_dens_mask |> m -> clamp.(m, 0, Inf)
+    dens_masked_cleaned = .!(mask1 .| mask2) |> calc_dens_mask
     x_modl, y_modl = essn.smwh_core |> s -> map(u -> (-u:1:u), s) |> xy -> xy .* essn.step_modl
     x_posi, y_posi = essn.smwh |> s -> map(u -> (-u:1:u), s) |> xy -> xy .* essn.step_posi
     x_posi_core, y_posi_core = essn.smwh_core |> s -> map(u -> (-u:1:u), s) |> xy -> xy .* essn.step_posi
     y_modl_sm = (0:1:essn.smwh_core[2]) * essn.step_modl[2]
     hue_theme = hue_theme_istp[info_solo["istp"]]
-    clrmap = gen_clrmap_solo(hue_theme)
-    clrmap_1 = gen_clrmap_solo(hue_1)
-    clrmap_2 = gen_clrmap_solo(hue_2)
+    clrmap = gen_clrmap_solo(hue_theme; thres_alpha=0.05, alpha_base=0.05)
+    clrmap_1 = gen_clrmap_solo(hue_1; thres_alpha=0.05, alpha_base=0.05)
+    clrmap_2 = gen_clrmap_solo(hue_2; thres_alpha=0.05, alpha_base=0.05)
     clr_mark_nvlp = RGBAf(Oklch(0.52, 0.10, hue_theme + 90), 1.0)
     clr_moments = Oklch(0.52, 0.14, hue_theme)
+    clr_prfl_masked = Oklch(0.52, 0.14, hue_theme)
 
     lims_core = (essn.smwh_core .+ 0.5) .* essn.step_posi |> l -> map(a -> [-a, a], l)
     lims_modl = ((0, 0.6), (-0.6, 0.6))
@@ -70,22 +81,25 @@ function draw_solo_modl_mask!(axs::Dict{String}, extr::SoloExtract, info_solo, m
 
     heatmap!(axs["dens_core"], x_posi_core, y_posi_core, (essn.dens2d_core .* gen_win_hann_2d(essn.smwh_core))'; colorrange=(0, dens_max), colormap=clrmap, rasterize=true)
     heatmap!(axs["dens_masked_1"], x_posi_core, y_posi_core, dens_masked_1'; colorrange=(0, dens_max), colormap=clrmap_1, rasterize=true)
-    # heatmap!(axs["dens_masked_2"], x_posi_core, y_posi_core, dens_masked_2'; colorrange=(0, dens_max), colormap=clrmap_2, rasterize=true)
+    heatmap!(axs["dens_masked_2"], x_posi_core, y_posi_core, dens_masked_2'; colorrange=(0, dens_max), colormap=clrmap_2, rasterize=true)
+    heatmap!(axs["dens_core_masked_in"], x_posi_core, y_posi_core, dens_masked_cleaned'; colorrange=(0, dens_max/2), colormap=clrmap, rasterize=true)
 
+    # prfl_masked = essn.modl2d .* mask_prfl |>
     clr_max_modl = dens_max * 5 / 8
     clr_modl_mask1 = to_masked_clr(modl2d_norm, mask1, hue_1; max=clr_max_modl, thres_alpha=0.0)
     clr_modl_mask2 = to_masked_clr(modl2d_norm, mask2, hue_2; max=clr_max_modl, thres_alpha=0.0)
-    heatmap!(axs["modl"], y_modl, x_modl, modl2d_norm; colorrange=(0, clr_max_modl), colormap=clrmap, rasterize=true)
+    clr_modl_mask_prfl = to_masked_clr(modl2d_norm, mask_prfl, hue_theme; max=clr_max_modl, thres_alpha=0.0)
+    clr_modl_mask_rest = to_masked_clr(modl2d_norm, (@. !(mask_prfl | mask1 | mask2)), hue_theme; max=clr_max_modl, thres_alpha=0.0, sat_max=0)
+    heatmap!(axs["modl"], y_modl, x_modl, clr_modl_mask_rest; rasterize=true)
     heatmap!(axs["modl"], y_modl, x_modl, clr_modl_mask1; rasterize=true)
     heatmap!(axs["modl"], y_modl, x_modl, clr_modl_mask2; rasterize=true)
-    hlines!(axs["modl"], (essn.smw_modl+0.5)*essn.step_modl[1]; color=(:black, 0.4), linewidth=1)
-    hlines!(axs["modl"], -(essn.smw_modl+0.5)*essn.step_modl[1]; color=(:black, 0.4), linewidth=1)
-    lines!(axs["upright"], y_modl_sm, essn.prfl_modl_norm_px[essn.smwh_core[2]+1:end], color=(:black, 0.4), linewidth=1)
-    lines!(axs["upright"], y_modl_sm, extr.sidepeak.prfl_norm_tailess_px[essn.smwh_core[2]+1:end], color=:black, linewidth=1)
-    # axs["modl"] |> hidedecorations!
-    axs["dens_core"] |> hidedecorations!
-    axs["upright"].yticklabelsvisible = false
-    axs["upright"].xticklabelsvisible = false
+    heatmap!(axs["modl"], y_modl, x_modl, clr_modl_mask_prfl; rasterize=true)
+    rng_prfl = essn.smwh_core[2] |> h -> h+1:(2*h + 1)
+    lines!(axs["upright"], y_modl, essn.prfl_modl_norm_px, color=(:black, 0.4), linewidth=1)
+    lines!(axs["upright"], y_modl, extr.sidepeak.prfl_norm_tailess_px, color=:black, linewidth=1)
+    lines!(axs["upright"], y_modl, prfls_masked.prfl_main_normed_px, color=:black, linestyle=:dash, linewidth=1)
+    lines!(axs["upright"], y_modl, prfls_masked.prfl_sidepeak_normed_px, color=clr_prfl_masked, linewidth=0.6)
+    lines!(axs["upright"], y_modl, prfls_masked.prfl_sidepeak_normed_px - fit_tail(y_modl), color=clr_prfl_masked, linewidth=1.4)
     limits!(axs["dens_core"], lims_core...)
     limits!(axs["dens_core_masked_in"], lims_core...)
     limits!(axs["dens_masked_1"], lims_core...)
@@ -96,12 +110,17 @@ function draw_solo_modl_mask!(axs::Dict{String}, extr::SoloExtract, info_solo, m
     vlines!(axs["upright"], 0.3; color=RGBAf(Oklch(0.3, 0, 0), 0.4))
     hlines!(axs["upright"], 0.0; color=(:darkseagreen1, 0.5))
     vlines!(axs["upright"], extr.sidepeak.params_tailess.wavenum; color=(:mediumspringgreen, 1.0))
-    # sp = extr.sidepeak.params_tailess
-    # make_dual = a -> [a, a]
-    # band!(axs["upright"], [mmt_coor_min, mmt_coor_max], 0 |> make_dual, -0.02 |> make_dual; color=(clr_moments, 1))
-    linkaxes!(axs["dens_core"], axs["dens_core_masked_in"], axs["dens_masked_1"], axs["dens_masked_2"])
+    linkaxes!(axs["dens_core"], axs["dens_core_masked_in"], axs["dens_masked_1"], axs["dens_masked_2"], axs["dens_core_masked_in"])
+    for ax in [axs["dens_core_masked_in"], axs["dens_masked_1"], axs["dens_masked_2"], axs["dens_core_masked_in"]]
+        ax.yticklabelsvisible = false
+        ax.xlabel = "μm"
+    end
+    axs["dens_core"].xlabel = "μm"
+    for ax in [axs["modl"], axs["upright"]]
+        ax.xlabel = "μm⁻¹"
+    end
     sprint2f = (x) -> @sprintf("%.2f", x)
-    axs["label"].text = "$(@sprintf("%.1f", info_solo["t_hold"])) ms | rep $(info_solo["repeat"])"
+    axs["label"].text = @sprintf("%.03f A | %.01f ms | rep %s", info_solo["IB"], info_solo["t_hold"], info_solo["repeat"])
 end
 
 function copy_symmetric_2d(modl::AbstractMatrix{<:Real})
@@ -124,25 +143,55 @@ end
 
 ## Loading
 ## Set a mask and show the results
+using GLMakie
+GLMakie.activate!()
 
-c, r, t, i = (4, 1, 10, 2)
+crti_samples = [
+    (4, 2, 19, 2, "strong fringe")
+    (4, 2,  1, 2, "messy envelop")
+    (4, 1, 30, 2, "small modulation peak")
+    (3, 2,  4, 2, "")
+    (3, 2,  3, 2, "small spacing")
+]
+fig = Figure()
+gl = GridLayout(fig[1, 1])
+axs = set_panel_solo_modl_masks!(gl)
+foreach(a -> a isa Axis && empty!(a), values(axs))
+
+c, r, t, i = (3, 3,  6, 2)
 extr = extr_fmt[c, r, t, i]
 info = info_fmt[c, r, t, i]
 essn = extr.essentials
+fit_tail = fit_prfl_modl_over_rep_t_1d[c, i].tail
 x_modl, y_modl = essn.smwh_core |> s -> map(u -> (-u:1:u), s) |> xy -> xy .* essn.step_modl
 x_posi, y_posi = essn.smwh |> s -> map(u -> (-u:1:u), s) |> xy -> xy .* essn.step_posi
 x_posi_core, y_posi_core = essn.smwh_core |> s -> map(u -> (-u:1:u), s) |> xy -> xy .* essn.step_posi
 
-fig = Figure()
-gl = GridLayout(fig[1, 1])
-
-axs = set_panel_solo_modl_masks!(gl)
-foreach(a -> a isa Axis && empty!(a), values(axs))
-
-mask_fringe = [((x - 0.37)/0.14)^2 + ((y - 0.32)/0.1)^2 < 1 for y in y_modl, x in x_modl] |> copy_symmetric_2d
-mask_envelop = fill(false, length(y_modl), length(x_modl))
-draw_solo_modl_mask!(axs, extr, info, mask_fringe, mask_envelop; dens_max=16.0, peak_height_max=2)
-
+mask_fringe = [((x - 0.37)/0.16)^2 + ((y - 0.32)/0.1)^2 < 1 for y in y_modl, x in x_modl] |> copy_symmetric_2d
+mask_envelop = [
+    begin
+        θ = -20 * π / 180
+        rot = [
+            cos(θ)  -sin(θ)
+            sin(θ)   cos(θ)
+        ]
+        x_r, y_r = rot * [x, y]
+        ((x_r/0.4)^2 + (y_r/0.15)^2 < 1) | ((x/0.55)^2 + (y/0.1)^2 < 1)
+    end
+    for y in y_modl, x in x_modl] |> copy_symmetric_2d
+mask_sidepeak = [@. abs(x) < 0.51 for y in y_modl, x in x_modl] |> m -> @. (m & !(mask_fringe | mask_envelop))
+mask_main = [@. (abs(x) < 0.51) for y in y_modl, x in x_modl] |> m -> @. (m & !mask_fringe)
+calc_prfl_masked = (modl, mask) -> sum(modl .* mask; dims=2) ./ sum(Int.(mask); dims=2) |> vec
+function calc_prfl_norm_px_masked(modl2d, mask_sp, mask_main, step_modl)
+    prfl_main = calc_prfl_masked(modl2d, mask_main)
+    prfl_sidepeak = calc_prfl_masked(modl2d, mask_sp)
+    norm_prfl_main = prfl_main |> sum |> s -> s * step_modl[2] / 2
+    prfl_main_normed_px = prfl_main ./ norm_prfl_main
+    prfl_sidepeak_normed_px = prfl_sidepeak ./ norm_prfl_main
+    (;norm_prfl_main, prfl_main_normed_px, prfl_sidepeak_normed_px)
+end
+prfls_masked = calc_prfl_norm_px_masked(essn.modl2d, mask_sidepeak, mask_main, essn.step_modl)
+draw_solo_modl_mask!(axs, extr, info, mask_fringe, mask_envelop, mask_sidepeak, prfls_masked, fit_tail; dens_max=16.0, peak_height_max=2)
 # norm_modl = m -> m ./ (sum(m) * (essn.step_modl[2] / 2)^2)
 # modl_fringe = essn.modl2d .* mask_fringe |> norm_modl
 # mask_hann = gen_win_hann_2d(smwh_core)
